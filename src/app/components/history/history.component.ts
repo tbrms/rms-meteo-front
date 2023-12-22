85fd14ced66cb1a2e8c7a5036a71d81b1df6a5e8
Steve Carré <kreynew@gmail.com>
2023-12-22 10:32:58 +0100

    change charts color

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, Signal, ViewChild, ViewChildren, WritableSignal, inject, signal } from '@angular/core';
+import { Component, OnInit, WritableSignal, inject, signal } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { MeteoService } from '../../services/meteo.service';
 import { Meteo } from '../../models/meteo';
 import { OopsComponent } from '../oops/oops.component';
-import { BaseChartDirective, NgChartsModule } from 'ng2-charts';
+import { NgChartsModule } from 'ng2-charts';
 import { ChartConfiguration, ChartOptions } from 'chart.js';
 import { CommonService } from '../../services/common.service';
-import { catchError, concatMap, delay, of, take, tap } from 'rxjs';
+import { catchError, concatMap, of, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-history',
@@ -30,15 +30,17 @@ export class HistoryComponent implements OnInit {
 
   ngOnInit(): void {
 
+
     /* L'extrait de code utilise les « params » observables à partir de « ActivatedRoute » pour obtenir
-    les paramètres de route. Il utilise ensuite l'opérateur `concatMap` pour enchaîner le résultat
-    de l'observable `this.meteoService.getHistoryWithDuration(params['duration'])`. */
+    les paramètres de route. Il utilise ensuite l'opérateur `concatMap` pour le chaîner avec la
+    méthode `getHistoryWithDuration` du `meteoService`. */
     this.route.params.pipe(
       concatMap((params: Params) => this.meteoService.getHistoryWithDuration(params['duration']).pipe(
         take(1),
         tap((result: Meteo[]) => {
           this.enoughData.set(result.length >= params['duration']);
           result.forEach((element: Partial<Meteo>) => {
+            // MAJ charts data
             const date = new Date(element.createdAt as Date);
             this.chartsLabels.unshift(
               `${this.commonService.hourOrMinuteWithTwoDigits(date.getHours())}h${this.commonService.hourOrMinuteWithTwoDigits(date.getMinutes())}`
@@ -57,8 +59,6 @@ export class HistoryComponent implements OnInit {
     ).subscribe();
   }
 
-
-
   protected temperatureChartData: ChartConfiguration<'line'>['data'] = {
     labels: this.chartsLabels,
     datasets: [
@@ -67,7 +67,7 @@ export class HistoryComponent implements OnInit {
         label: 'Température °C',
         fill: true,
         borderColor: 'black',
-        backgroundColor: 'rgba(255,0,0,0.3)'
+        backgroundColor: 'rgba(255,0,0,0.8)'
       }
     ]
   };
@@ -80,20 +80,21 @@ export class HistoryComponent implements OnInit {
         label: 'Presion hPa',
         fill: true,
         borderColor: 'black',
-        backgroundColor: 'rgba(255,0,0,0.3)'
+        backgroundColor: 'rgba(0,0,0,0.3)'
       }
     ]
   };
 
   protected humidityChartData: ChartConfiguration<'line'>['data'] = {
     labels: this.chartsLabels,
+    yLabels: [0,10,20,30,40,50,60,70,80,90,100],
     datasets: [
       {
         data: this.humidityData,
         label: 'Humidité %',
         fill: true,
         borderColor: 'black',
-        backgroundColor: 'rgba(255,0,0,0.3)'
+        backgroundColor: 'rgba(0,0,255,0.7)'
       }
     ]
   };
@@ -101,6 +102,7 @@ export class HistoryComponent implements OnInit {
   protected lineChartOptions: ChartOptions<'line'> = {
     responsive: false
   };
+
   protected lineChartLegend = true;
 
   /**
