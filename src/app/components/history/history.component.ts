0d1bbfe1535484c1620619730cec61e93de31252
Steve Carré <kreynew@gmail.com>
2023-12-22 09:43:01 +0100

    fix bug load history data

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Signal, inject } from '@angular/core';
+import { Component, OnInit, Signal, ViewChild, ViewChildren, WritableSignal, inject, signal } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { MeteoService } from '../../services/meteo.service';
 import { Meteo } from '../../models/meteo';
 import { OopsComponent } from '../oops/oops.component';
-import { NgChartsModule } from 'ng2-charts';
+import { BaseChartDirective, NgChartsModule } from 'ng2-charts';
 import { ChartConfiguration, ChartOptions } from 'chart.js';
 import { CommonService } from '../../services/common.service';
+import { catchError, concatMap, delay, of, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-history',
@@ -20,33 +21,42 @@ export class HistoryComponent implements OnInit {
   private readonly meteoService: MeteoService = inject(MeteoService);
   private readonly commonService: CommonService = inject(CommonService);
 
-  protected meteoHistory: Signal<Partial<Meteo>[]> = this.meteoService.meteoHistory;
-  protected enoughData: Signal<boolean> = this.meteoService.enoughData;
+  protected enoughData: WritableSignal<boolean> = signal(false);
 
   protected chartsLabels: string[] = [];
   protected temperatureData: number[] = [];
   protected pressionData: number[] = [];
   protected humidityData: number[] = [];
 
-
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
-      this.meteoService.getHistory(params['duration']);
-      this.meteoHistory().forEach((element: Partial<Meteo>) => {
-        const date = new Date(element.createdAt as Date);
-        this.chartsLabels.unshift(
-          `${this.commonService.hourOrMinuteWithTwoDigits(date.getHours())}h${this.commonService.hourOrMinuteWithTwoDigits(date.getMinutes())}`
-        );
-        this.temperatureData.unshift(element.temperature as number);
-        this.pressionData.unshift(element.pression as number);
-        this.humidityData.unshift(element.humidite as number);
-      });
-      this.chartsLabels = [];
-      this.temperatureData = [];
-      this.pressionData = [];
-      this.humidityData = [];
-    });
+
+    /* L'extrait de code utilise les « params » observables à partir de « ActivatedRoute » pour obtenir
+    les paramètres de route. Il utilise ensuite l'opérateur `concatMap` pour enchaîner le résultat
+    de l'observable `this.meteoService.getHistoryWithDuration(params['duration'])`. */
+    this.route.params.pipe(
+      concatMap((params: Params) => this.meteoService.getHistoryWithDuration(params['duration']).pipe(
+        take(1),
+        tap((result: Meteo[]) => {
+          this.enoughData.set(result.length >= params['duration']);
+          result.forEach((element: Partial<Meteo>) => {
+            const date = new Date(element.createdAt as Date);
+            this.chartsLabels.unshift(
+              `${this.commonService.hourOrMinuteWithTwoDigits(date.getHours())}h${this.commonService.hourOrMinuteWithTwoDigits(date.getMinutes())}`
+            );
+            this.temperatureData.unshift(element.temperature as number);
+            this.pressionData.unshift(element.pression as number);
+            this.humidityData.unshift(element.humidite as number);
+          });
+          this.resetCharts();
+        }),
+        catchError(error => {
+          console.error(`an error has occurred: ${error}`);
+          return of('an error has occurred');
+        })
+      ))
+    ).subscribe();
   }
+
 
 
   protected temperatureChartData: ChartConfiguration<'line'>['data'] = {
@@ -93,4 +103,14 @@ export class HistoryComponent implements OnInit {
   };
   protected lineChartLegend = true;
 
+  /**
+   * La fonction réinitialise les tableaux de données utilisés pour les graphiques de température, de
+   * pression et d'humidité.
+   */
+  private resetCharts(): void {
+    this.chartsLabels = [];
+    this.temperatureData = [];
+    this.pressionData = [];
+    this.humidityData = [];
+  }
 }
--- src/app/services/meteo.service.ts
+++ src/app/services/meteo.service.ts
@@ -1,7 +1,7 @@
 import {inject, Injectable, signal, WritableSignal} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Meteo} from "../models/meteo";
-import {catchError, of, take, tap} from "rxjs";
+import {catchError, Observable, of, take, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +12,6 @@ export class MeteoService {
   private readonly baseUrl: string = 'http://localhost:3000/meteo';
 
   meteoNow: WritableSignal<Partial<Meteo>> = signal({});
-  meteoHistory: WritableSignal<Partial<Meteo>[]> = signal([]);
-  enoughData: WritableSignal<boolean> = signal(false);
 
   constructor() {
     this.getNow();
@@ -37,22 +35,14 @@ export class MeteoService {
   }
 
   /**
-   * La fonction récupère les données météorologiques historiques pour une durée spécifiée et les
-   * stocke dans une variable.
-   * @param {number} duration - Le paramètre de durée est un nombre qui représente la période pour
-   * laquelle vous souhaitez récupérer les données de l'historique. Il s'agit du nombre d'heures.
+   * La fonction `getHistoryWithDuration` renvoie un observable de type `Meteo[]` en effectuant une
+   * requête HTTP GET vers une URL spécifiée avec une durée donnée.
+   * @param {number} duration - Le paramètre de durée est un nombre qui représente la durée pendant
+   * laquelle vous souhaitez récupérer l'historique météo. Cela peut être en minutes, heures, jours ou
+   * toute autre unité de temps selon la façon dont l'API backend est conçue.
+   * @returns un observable de type Meteo[].
    */
-  getHistory(duration: number): void {
-    this.httpClient.get<Meteo[]>(`${this.baseUrl}/history/${duration}`).pipe(
-      take(1),
-      tap((response: Meteo[]) => {
-        this.enoughData.set(response.length >= duration);
-        this.meteoHistory.set(response);
-      }),
-      catchError(error => {
-        console.error(`an error has occurred: ${error}`);
-        return of('an error has occurred');
-      })
-    ).subscribe();
+  getHistoryWithDuration(duration: number): Observable<Meteo[]> {
+    return this.httpClient.get<Meteo[]>(`${this.baseUrl}/history/${duration}`);
   }
 }
